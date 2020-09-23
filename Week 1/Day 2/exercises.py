2a50946a6141cc07db6a2b83f2fc98699476fc05
Eugene Birondo <jbirondo@gmail.com>
2020-09-23 00:31:05 -0700

    First Before Second

--- Week 1/Day 2/exercises.py
+++ Week 1/Day 2/exercises.py
@@ -240,32 +240,52 @@
 # ---
 # Write your function, here.
 
-def is_valid_hex_code(str):
-    if str[0] != "#":
-        return False
-    if len(str) != 7:
-        return False
-    for letter in str[1:]:
-        if letter.upper() not in "ABCDEF1234567890":
-            return False
-    return True
+# def is_valid_hex_code(str):
+#     if str[0] != "#":
+#         return False
+#     if len(str) != 7:
+#         return False
+#     for letter in str[1:]:
+#         if letter.upper() not in "ABCDEF1234567890":
+#             return False
+#     return True
 
-print(is_valid_hex_code("#CD5C5C"))  # > True
-print(is_valid_hex_code("#EAECEE"))  # > True
-print(is_valid_hex_code("#eaecee"))  # > True
+# print(is_valid_hex_code("#CD5C5C"))  # > True
+# print(is_valid_hex_code("#EAECEE"))  # > True
+# print(is_valid_hex_code("#eaecee"))  # > True
 
-print(is_valid_hex_code("#CD5C58C"))
-# Prints False
-# Length exceeds 6
+# print(is_valid_hex_code("#CD5C58C"))
+# # Prints False
+# # Length exceeds 6
 
-print(is_valid_hex_code("#CD5C5Z"))
-# Prints False
-# Not all alphabetic characters in A-F
+# print(is_valid_hex_code("#CD5C5Z"))
+# # Prints False
+# # Not all alphabetic characters in A-F
 
-print(is_valid_hex_code("#CD5C&C"))
-# Prints false
-# Contains unacceptable character
+# print(is_valid_hex_code("#CD5C&C"))
+# # Prints false
+# # Contains unacceptable character
 
-print(is_valid_hex_code("CD5C5C"))
-# Prints False
-# Missing #
+# print(is_valid_hex_code("CD5C5C"))
+# # Prints False
+# # Missing #
+
+# ---
+
+# Write your function, here.
+def first_before_second(str, el1, el2):
+    return(len(str) - str[::-1].find(el1) < str.find(el2))
+
+print(first_before_second("a rabbit jumps joyfully", "a", "j"))
+#> True
+# Every instance of "a" occurs before every instance of "j".
+
+print(first_before_second("knaves knew about waterfalls", "k", "w"))
+#> True
+
+print(first_before_second("happy birthday", "a", "y"))
+#> False
+# The "a" in "birthday" occurs after the "y" in "happy".
+
+print(first_before_second("precarious kangaroos", "k", "a"))
+
