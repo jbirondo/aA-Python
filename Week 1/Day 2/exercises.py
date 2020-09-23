21c75593ed9edae2217470d618dcceb0689c7442
Eugene Birondo <jbirondo@gmail.com>
2020-09-22 22:52:15 -0700

    Min to Sec

--- Week 1/Day 2/exercises.py
+++ Week 1/Day 2/exercises.py
@@ -74,10 +74,14 @@
 
 # Your function here
 
-def min2sec(num):
-    return num * 60
+# def min2sec(num):
+#     return num * 60
 
 
-print(min2sec(5))  # > 300
-print(min2sec(3))  # > 180
-print(min2sec(2))  # > 120
+# print(min2sec(5))  # > 300
+# print(min2sec(3))  # > 180
+# print(min2sec(2))  # > 120
+
+# ---
+
+
