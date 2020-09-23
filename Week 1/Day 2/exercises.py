1d817ff4d8ae5ea251884c060d6e387141891d21
Eugene Birondo <jbirondo@gmail.com>
2020-09-22 22:53:52 -0700

    How Many Legs

--- Week 1/Day 2/exercises.py
+++ Week 1/Day 2/exercises.py
@@ -84,12 +84,12 @@
 
 # ---
 
-# Write your function, here.
-# Parameters are in this order: chickens, cows, pigs
+# # Write your function, here.
+# # Parameters are in this order: chickens, cows, pigs
 
-def how_many_legs(chickens, cows, pigs):
-    return(chickens * 2 + cows * 4 + pigs * 4)
+# def how_many_legs(chickens, cows, pigs):
+#     return(chickens * 2 + cows * 4 + pigs * 4)
 
-print(how_many_legs(2, 3, 5))  # > 36
-print(how_many_legs(1, 2, 3))  # > 22
-print(how_many_legs(5, 2, 8))  # > 50
+# print(how_many_legs(2, 3, 5))  # > 36
+# print(how_many_legs(1, 2, 3))  # > 22
+# print(how_many_legs(5, 2, 8))  # > 50
