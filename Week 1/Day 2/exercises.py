b0caefb1dc636466e6bae2b688cf728113c1cc60
Eugene Birondo <jbirondo@gmail.com>
2020-09-22 21:43:56 -0700

    Week 1 Day 2

---
+++ Week 1/Day 2/exercises.py
@@ -0,0 +1,14 @@
+print("** Doubling Penny **")
+
+# How many times would a penny need to double to generate a million dollars?
+count = 0
+total = 0
+
+# STEP 2: Write the while loop
+while total < 100000000:
+
+
+print('Double', count, 'times')
+
+# How much money has been generated at that point?
+print('${:,}'.format(total))
