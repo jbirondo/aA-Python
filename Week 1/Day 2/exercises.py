162fa2c33e5f20afd54b147fe7e79decc1a6afd3
Eugene Birondo <jbirondo@gmail.com>
2020-09-23 00:01:56 -0700

    Last Letter N

--- Week 1/Day 2/exercises.py
+++ Week 1/Day 2/exercises.py
@@ -214,3 +214,15 @@
 # print(divisible_by_five(37))  # > False
 
 # # ---
+
+# # Write your function, here.
+
+# def is_last_character_n(str):
+#     return str[-1] == "n"
+
+# print(is_last_character_n("Aiden"))  # > True
+# print(is_last_character_n("Piet"))  # > False
+# print(is_last_character_n("Bert"))  # > False
+# print(is_last_character_n("Dean"))  # > True
+
+# ---
