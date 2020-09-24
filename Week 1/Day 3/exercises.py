2642ffae9c288e706909497c3bb39b6f3bef2a0d
Eugene Birondo <jbirondo@gmail.com>
2020-09-23 23:40:19 -0700

    Range Loops

--- Week 1/Day 3/exercises.py
+++ Week 1/Day 3/exercises.py
@@ -114,24 +114,31 @@
 # nums = 123,234,345,456,567
 # print(minmaxmean(nums))
 
-print('THREE CASES FOR RANGE')
-print('A) End Value')
+# print('THREE CASES FOR RANGE')
+# print('A) End Value')
 
-# STEP 1: Change the zero in the range to 10
-#         Notice how "10" is not included in the output
-for i in range(10):
-    print(i)
+# # STEP 1: Change the zero in the range to 10
+# #         Notice how "10" is not included in the output
+# for i in range(10):
+#     print(i)
 
-print('B) Start & End Values')
+# print('B) Start & End Values')
 
-# STEP 2: Code a `for` loop to print numbers 5 through 9
-for i in range(5, 10):
-    print(i)
+# # STEP 2: Code a `for` loop to print numbers 5 through 9
+# for i in range(5, 10):
+#     print(i)
 
-print('C) Only Even Values')
+# print('C) Only Even Values')
 
-# STEP 3: Print 0, 2, 4, 6 and 8 using a for loop
-#         Hint - range can take a 3rd parameter for the step distance
+# # STEP 3: Print 0, 2, 4, 6 and 8 using a for loop
+# #         Hint - range can take a 3rd parameter for the step distance
 
-for i in range(0, 9, 2):
-    print(i)
+# for i in range(0, 9, 2):
+#     print(i)
+
+# Powers of 2 from 1 to 16
+# Write a for loop that uses the range function to
+# print the powers of 2 from 2 - 65536, that is
+# from 2^1st - 2^16th powers
+for i in range(1, 17):
+    print(2 ** i)
