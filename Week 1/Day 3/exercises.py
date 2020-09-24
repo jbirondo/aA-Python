5b2351ddfc0e37fbc4bf747bdd1fb910f29f6314
Eugene Birondo <jbirondo@gmail.com>
2020-09-23 23:54:37 -0700

    Check Nested Arrays

--- Week 1/Day 3/exercises.py
+++ Week 1/Day 3/exercises.py
@@ -184,12 +184,12 @@
 
 # Your code, here.
 
-def can_nest(list1, list2):
-    if min(list1) > min(list2) and max(list1) < max(list2):
-        return True
-    return False
+# def can_nest(list1, list2):
+#     if min(list1) > min(list2) and max(list1) < max(list2):
+#         return True
+#     return False
 
-print(can_nest([1, 2, 3, 4], [0, 6]))  # > True
-print(can_nest([3, 1], [4, 0]))  # > True
-print(can_nest([9, 9, 8], [8, 9]))  # > False
-print(can_nest([1, 2, 3, 4], [2, 3]))  # > False
+# print(can_nest([1, 2, 3, 4], [0, 6]))  # > True
+# print(can_nest([3, 1], [4, 0]))  # > True
+# print(can_nest([9, 9, 8], [8, 9]))  # > False
+# print(can_nest([1, 2, 3, 4], [2, 3]))  # > False
