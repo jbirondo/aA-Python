01a0025a01a36bf7b8faa475831029443eee98d9
Eugene Birondo <jbirondo@gmail.com>
2020-09-23 23:33:10 -0700

    Explore the Dictionary

--- Week 1/Day 3/exercises.py
+++ Week 1/Day 3/exercises.py
@@ -74,3 +74,42 @@
 # #  "1"         should be True
 # #  "one"       should be False
 # #  True        should be False
+
+# DO NOT EDIT
+odds = 1, 3, 5, 7, 9
+evens = 2, 4, 6, 8
+
+# Step 1: Print out the result of adding evens to odds
+print(evens + odds)
+
+# Step 2: Print out the result of multiplying odds by three
+print([x * 3 for x in odds])
+
+# Step 3A: Use print to find out if odds is less than evens
+print(odds < evens)
+
+# Step 3B: Print out your explanation of why 3A has that result
+print("Tuples are compared position by position")
+
+# Step 4: Print out the average of the numbers in evens using one line of code
+print(sum(evens) / len(evens))
+
+# Step 5A: Write a function 'minmaxmean' that accepts an iterable and
+#         returns the minimum value, the maximum value and the average (mean)
+
+def minmaxmean(iter):
+    mini = min(iter)
+    maxi = max(iter)
+    mean = sum(iter) / len(iter)
+    return("Min = {}, Max = {}, Mean = {}".format(mini, maxi, mean))
+
+# Step 5B: Use print to confirm you function is working on evens and odds
+
+print(minmaxmean(evens))
+print(minmaxmean(odds))
+
+# BONUS: Call your function with a new tuple of your own creation
+#        And print the results in a pretty way
+
+nums = 123,234,345,456,567
+print(minmaxmean(nums))
