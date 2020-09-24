5b32673fd11141090e7f64c38b1f33770f2bf280
Eugene Birondo <jbirondo@gmail.com>
2020-09-23 23:38:21 -0700

    Explore the Range

--- Week 1/Day 3/exercises.py
+++ Week 1/Day 3/exercises.py
@@ -113,3 +113,25 @@
 
 # nums = 123,234,345,456,567
 # print(minmaxmean(nums))
+
+print('THREE CASES FOR RANGE')
+print('A) End Value')
+
+# STEP 1: Change the zero in the range to 10
+#         Notice how "10" is not included in the output
+for i in range(10):
+    print(i)
+
+print('B) Start & End Values')
+
+# STEP 2: Code a `for` loop to print numbers 5 through 9
+for i in range(5, 10):
+    print(i)
+
+print('C) Only Even Values')
+
+# STEP 3: Print 0, 2, 4, 6 and 8 using a for loop
+#         Hint - range can take a 3rd parameter for the step distance
+
+for i in range(0, 9, 2):
+    print(i)
