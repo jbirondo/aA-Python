a5db02c51101a4dd54e4eeea301445c4e348140c
Eugene Birondo <jbirondo@gmail.com>
2020-09-24 00:53:55 -0700

    Guess The Number

--- Week 1/Day 3/guess_a_number.py
+++ Week 1/Day 3/guess_a_number.py
@@ -7,19 +7,23 @@ rand = random.randint(1, 20)
 user_name = input("What is your name?: ")
 
 print("Well, {}, I am thinking of a number between 1 and 20.".format(user_name))
-guess = input("Take a guess: ")
 
-while num_guesses < max_guesses:
-    if int(guess) < int(rand):
+while num_guesses <= max_guesses:
+    guess = input("Take a guess: ")
+    if num_guesses == max_guesses:
+        print("Sorry, {}. You could not guess my number {}.".format(user_name, rand))
+        break
+    elif int(guess) < int(rand):
         num_guesses += 1
         print("Guess is too low.")
-        guess = input("Take a guess: ")
     elif int(guess) > int(rand):
         num_guesses += 1
         print("Guess is too high.")
-        guess = input("Take a guess: ")
     elif int(guess) == int(rand):
         num_guesses += 1
         print("Good job {}! You guessed the number in {} guesses!".format(user_name, num_guesses))
         break
+
+
+
 
