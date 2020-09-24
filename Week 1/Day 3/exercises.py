bfcec30e5bdd5f688e01c2177d21f1dca5e3facb
Eugene Birondo <jbirondo@gmail.com>
2020-09-23 23:47:21 -0700

    Get Indices

--- Week 1/Day 3/exercises.py
+++ Week 1/Day 3/exercises.py
@@ -162,22 +162,22 @@
 
 # Write your function, here.
 
-def get_indices(l, c):
-    arr = []
-    for x in range(0, len(l)):
-        if l[x] == c:
-            arr.append(x)
-    return arr
+# def get_indices(l, c):
+#     arr = []
+#     for x in range(0, len(l)):
+#         if l[x] == c:
+#             arr.append(x)
+#     return arr
 
 
-print(get_indices(["a", "a", "b", "a", "b", "a"], "a"))
-# Prints [0, 1, 3, 5]
+# print(get_indices(["a", "a", "b", "a", "b", "a"], "a"))
+# # Prints [0, 1, 3, 5]
 
-print(get_indices([1, 5, 5, 2, 7], 7))
-# Prints [4]
+# print(get_indices([1, 5, 5, 2, 7], 7))
+# # Prints [4]
 
-print(get_indices([1, 5, 5, 2, 7], 5))
-# Prints [1, 2]
+# print(get_indices([1, 5, 5, 2, 7], 5))
+# # Prints [1, 2]
 
-print(get_indices([1, 5, 5, 2, 7], 8))
-# Prints []
+# print(get_indices([1, 5, 5, 2, 7], 8))
+# # Prints []
