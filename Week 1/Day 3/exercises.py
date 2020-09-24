bfeb637a89926bc180cd8f10cd55c9efb5926766
Eugene Birondo <jbirondo@gmail.com>
2020-09-23 23:09:55 -0700

    Explore the List

---
+++ Week 1/Day 3/exercises.py
@@ -0,0 +1,34 @@
+# # DO NOT EDIT - Starting with a simple lists of colors and numbers
+# colors = ["blue", "Green", "PURPLE", "blue-green", "sky blue"]
+# numbers = [2, 34, 8.5, -22.0, 33//4, 2**5]
+# print('COLORS', colors)
+# print('NUMBERS', numbers)
+
+# # 1. Print the total number of colors (length of the list)
+# print(len(colors))
+
+# # 2. Print the first color
+# print(colors[0])
+
+# # 3. Print the second and third colors
+# print(colors[1:3])
+
+# # 4. Print the last two colors
+# print(colors[-2:])
+
+# # 5. Print the smallest number in the numbers list
+# print(sorted(numbers)[0])
+
+# # 6. Print the largest number in the numbers list
+# print(sorted(numbers, reverse=True)[0])
+
+# # 7. Sort the numbers
+
+# numbers = sorted(numbers)
+# # UNCOMMENT WHEN YOU WORK ON #7
+# print ('SORTED NUMBERS', numbers)
+
+# # 8. Sort the colors alphabetically ignoring case
+# colors = sorted(colors, key=lambda x: x.lower())
+# # UNCOMMENT WHEN YOU WORK ON #8
+# print ('SORTED COLORS', colors)
