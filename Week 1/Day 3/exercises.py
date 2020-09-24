dd42814148adc814b21d55b1e11575f597790551
Eugene Birondo <jbirondo@gmail.com>
2020-09-24 11:23:59 -0700

    Track Robot

--- Week 1/Day 3/exercises.py
+++ Week 1/Day 3/exercises.py
@@ -278,3 +278,33 @@
 # print(find_digit_amount(7154))  # > 4
 # print(find_digit_amount(61217311514))  # > 11
 # print(find_digit_amount(0))  # > 1
+
+# Write your function, here.
+
+def track_robot(l):
+    moves = [0, 0]
+    for x in l:
+        a = x.split()[0]
+        b = x.split()[1]
+        if a == "right":
+            moves[0] += int(b)
+        if a == "left":
+            moves[0] -= int(b)
+        if a == "up":
+            moves[1] += int(b)
+        if a == "down":
+            moves[1] -= int(b)
+    return moves
+
+        
+
+
+print(track_robot(["right 10", "up 50", "left 30", "down 10"]))
+# Prints [-20, 40]
+
+print(track_robot([]))
+# Prints [0, 0]
+# If there are no instructions, the robot doesn't move.
+
+print(track_robot(["right 100", "right 100", "up 500", "up 10000"]))
+# Prints [200, 10500]
