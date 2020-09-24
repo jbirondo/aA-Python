49d6292aea8b9fc44c61b914635d271c917ff0f0
Eugene Birondo <jbirondo@gmail.com>
2020-09-23 23:47:04 -0700

    Get Indices

--- Week 1/Day 3/exercises.py
+++ Week 1/Day 3/exercises.py
@@ -159,3 +159,25 @@
 # print(get_sum_of_elements([2, 7, 4]))  # > 13
 # print(get_sum_of_elements([45, 3, 0]))  # > 48
 # print(get_sum_of_elements([-2, 84, 23]))  # > 105
+
+# Write your function, here.
+
+def get_indices(l, c):
+    arr = []
+    for x in range(0, len(l)):
+        if l[x] == c:
+            arr.append(x)
+    return arr
+
+
+print(get_indices(["a", "a", "b", "a", "b", "a"], "a"))
+# Prints [0, 1, 3, 5]
+
+print(get_indices([1, 5, 5, 2, 7], 7))
+# Prints [4]
+
+print(get_indices([1, 5, 5, 2, 7], 5))
+# Prints [1, 2]
+
+print(get_indices([1, 5, 5, 2, 7], 8))
+# Prints []
